bb739c88f2c52f7d9ebcef2fd4977a0b4bb3120b
Toughee <MJ1menez@protonmail.com>
2020-08-20 18:42:32 -0500

    combY/003/Documentation_update - Fixed issue with documentation for pull requests. PR template would not appear beforehand - Added pseudocode for website scraping app

--- combY.py
+++ combY.py
@@ -9,7 +9,11 @@ CombYHtml = CombYFile.read()
 CombYFile.close()
 
 search = BeautifulSoup(CombYHtml, "lxml")
-#CombYAll = search.find_all("a", {"class": "storylink"})
-#for hyperlinks in search.find_all('a'):
-#    print (hyperlinks.get('href'))
-# Missing text file output
+CombYAll = search.find_all("a", {"class": "storylink"})
+for hyperlinks in search.find_all('a'):
+    print (hyperlinks.get('href'))
+# Currently missing text file output.  (listings.txt)
+# Current code lists all text lines with the a href property 
+# Code should output 
+# 1) Listing name for articles
+# 2) Url for listed articles
